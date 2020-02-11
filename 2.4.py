684ff113eba4d334d2db4648849580916d7b8903
tetyorkin <tetyorkin@gmail.com>
2020-02-11 18:13:21 +0600

    add regex

--- 2.4.py
+++ 2.4.py
@@ -1,8 +1,13 @@
+import re
 def open_file(name_file):
+    regex = re.compile(r"(?P<head>^.+\n(?P<id>\d))", re.MULTILINE)
     with open(name_file, encoding='utf8') as file:
-       pass
+        for match in regex.finditer(file.read()):
+            r = []
+            r += [{match.group('head'): match.group('head')}]
 
 
+            print(r)
 
 
 
