cda63e7fe308aa10fa4919346c480913865981e8
tetyorkin <tetyorkin@gmail.com>
2020-02-17 14:43:16 +0600

    del workflow

--- Functions classes file/2.4.py
+++ Functions classes file/2.4.py
@@ -1,20 +1,13 @@
+import re
+
+
 def make_cook_book(name_file):
-    cook_book = {}
-    with open(name_file, encoding='utf-8') as file:
-        for line in file:
-            head = line.strip()
-            count_ingredients = int(file.readline().strip())
-            ingredients = []
-            for item in range(count_ingredients):
-                ingredient = file.readline().split('|')
-                all_ingredients = {'ingredient_name': ingredient[0].strip(),
-                                   'quantity': int(ingredient[1]),
-                                   'measure': ingredient[2].strip()}
-                ingredients.append(all_ingredients)
-            file.readline()
-            cook_book.update({head: ingredients})
-    return cook_book
+    regex = re.compile(r"(?P<head>^.+\n(?P<id>\d))", re.MULTILINE)
+    with open(name_file, encoding='utf8') as file:
+        for match in regex.finditer(file.read()):
+            r = []
+            r += [{match.group('head'): match.group('head')}]
 
 
 if __name__ == "__main__":
-    print(make_cook_book('recipes.txt'))
+    make_cook_book('recipes.txt')
