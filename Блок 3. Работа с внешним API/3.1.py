d68b9f95f25d5d2bab326ebcc6625c044fe30cc1
tetyorkin <tetyorkin@gmail.com>
2020-02-19 17:10:12 +0600

    готов отсортированный список

--- Блок 3. Работа с внешним API/3.1.py
+++ Блок 3. Работа с внешним API/3.1.py
@@ -5,7 +5,14 @@ def read_file(file_name):
     with open(file_name, encoding='utf-8') as f:
         file_content = f.read()
         templates = json.loads(file_content)
-        print(templates)
+        new = templates['rss']['channel']['items']
+        big_world = []
+        for key in new:
+            item = key['description'].split()
+            for word in item:
+                if len(word) > 6:
+                    big_world.append(word.lower().split())
+        big_world_sorted = sorted(big_world)
 
 
 if __name__ == '__main__':
