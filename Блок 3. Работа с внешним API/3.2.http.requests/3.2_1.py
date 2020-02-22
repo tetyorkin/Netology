11305b34d9bee47427bef8e0140828cd85a0164f
tetyorkin <tetyorkin@gmail.com>
2020-02-23 01:20:30 +0600

    Задача 1 выполнена

--- Блок 3. Работа с внешним API/3.2.http.requests/3.2_1.py
+++ Блок 3. Работа с внешним API/3.2.http.requests/3.2_1.py
@@ -1,34 +1,89 @@
 import requests
-#  документация https://yandex.ru/dev/translate/doc/dg/reference/translate-docpage/
+import os
 
+FILE_DE = 'DE.txt'
+FILE_ES = 'ES.txt'
+FILE_FR = 'FR.txt'
+FILE_SUPPORT = 'support_lang.txt'
 API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+URL1 = 'https://translate.yandex.net/api/v1.5/tr.json/getLangs'
 
-def translate_it(text, to_lang):
-    """
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param to_lang:
-    :return:
-    """
 
+# Изначально написал функцию проверяющую поддержку языка перевода. Но API Яндекса возвращает только 33
+# поддерживаемых языка. Хотя на самом деле их 93. Добавил файл со списком поддерживаемых языков
+#  Функция support_lang() считывает файл со списком поддерживаемых языков и возвращает словарь
+def support_lang(file):
+    with open(file, encoding='utf-8') as file:
+        support = {}
+        for line in file:
+            dict_lang = {line.split()[0]: line.split()[1]}
+            support.update(dict_lang)
+    return support
+
+
+# Отрываем и читаем файл, вовращвем его как строку
+def open_read_file(file):
+    with open(file, encoding='utf-8') as file:
+        read = file.readlines()
+        buf = ','.join(read)
+        text = buf.replace('\n,', '\n')
+    return text
+
+
+# Определяем язык файла по названию файла
+def lang(file):
+    language = file[0:2].lower()
+    return language
+
+
+# Перводим текст, в нужном направлении и возвращвем перевод
+def translate_it(file, to_lang):
+    text = open_read_file(file)
+    from_lang = lang(file)
     params = {
         'key': API_KEY,
         'text': text,
-        'lang': 'ru-{}'.format(to_lang),
+        'lang': f"{from_lang}-{to_lang}"
     }
-
     response = requests.get(URL, params=params)
-    json_ = response.json()
-    return ''.join(json_['text'])
+    translated_list = response.json()['text']
+    translated_text = ','.join(translated_list)
+    return translated_text
 
 
-# print(translate_it('В настоящее время доступна единственная опция — признак включения в ответ автоматически определенного языка переводимого текста. Этому соответствует значение 1 этого параметра.', 'no'))
+# Выбор языка на который хотим перевести
+def choose_lang(to_lang):
+    if to_lang == '':
+        to_lang = 'ru'
+    return to_lang
 
+
+# Если нет файла создаём, если есть, записываем результат перевода в конец файла.
+# Перевод всех файлов пишется в один новый файл
+def write_to_file(file):
+    with open(new_file_name, 'a', encoding='utf-8') as f:
+        to_language = str(choose_lang(input_lang))
+        translate_text = translate_it(file, to_language)
+        f.writelines(f'-----------------------------------\n'
+                     f'translate from {lang(file).upper()} to {to_language.upper()}\n'
+                     f'{translate_text}\n')
+
+
+# input_lang - пользователь выбирает язык на который необходимо перевести текст.
+# На основе выбора языка выбирается имя нового файла, если файл с таким именем уже существует, он удаляется.
 if __name__ == '__main__':
-    print(translate_it('привет', 'en'))
+    support_lang = support_lang(FILE_SUPPORT)
+    input_lang = input(f"На какой язык произвести перевод? (Без указания, будет выбран русский язык)\n"
+                       f"Для перевода доступны следующие языки: {support_lang}\n")
+    if input_lang in support_lang.values() or input_lang == '':
+        new_file_name = f"translate_to_{choose_lang(input_lang).upper()}.txt"
+        if os.path.exists(new_file_name):
+            os.remove(new_file_name)
+        choose_lang(input_lang)
+        write_to_file(FILE_DE)
+        write_to_file(FILE_ES)
+        write_to_file(FILE_FR)
+        print('Файл с переводом готов')
+    else:
+        print(f"К сожалению  выбранный язык не поддерживается")
